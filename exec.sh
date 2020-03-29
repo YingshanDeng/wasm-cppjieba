371e871658b5a21f9c7b142e7a3a347dc26dbe60
YingshanDeng <yingshandeng@gmail.com>
2020-03-29 19:52:37 +0800

    feat: cppjieba cmake

---
+++ exec.sh
@@ -0,0 +1,14 @@
+
+DIR="build"
+if [ ! -d "$DIR" ]; then
+    ###  Create if $DIR does NOT exists ###
+    echo "Create ${DIR} folder"
+    mkdir ${DIR}
+else
+    echo "${DIR} folder exists"
+fi
+
+echo "Enter ${DIR} exec cmake .. && make"
+cd build
+cmake ..
+make
