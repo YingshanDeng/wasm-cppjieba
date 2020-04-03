4599282cddc85f90c3618e0834ee5f8d7274295f
YingshanDeng <yingshandeng@gmail.com>
2020-04-03 19:43:20 +0800

    feat: 移除无用代码

--- script/build-cppjieba.sh
+++ script/build-cppjieba.sh
@@ -1,13 +1,8 @@
 DIR="build"
 if [ ! -d "$DIR" ]; then
-    ###  Create if $DIR does NOT exists ###
-    echo "Create ${DIR} folder"
     mkdir ${DIR}
-else
-    echo "${DIR} folder exists"
 fi
 
-echo "Enter ${DIR} exec cmake .. && make"
 cd build
 emcmake cmake ..
 emmake make
--- script/build-dicts-idbfs.sh
+++
@@ -1,2 +0,0 @@
-emcc src/dicts-idbfs.c -o dist/index.html -s FORCE_FILESYSTEM=1 -lidbfs.js
-emrun --no_browser --port 8081 dist
