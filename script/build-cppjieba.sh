d6b5a0d717afb76abc0008c46671544a749d3a61
YingshanDeng <yingshandeng@gmail.com>
2020-04-05 21:21:16 +0800

    feat: update build shell

--- script/build-cppjieba.sh
+++ script/build-cppjieba.sh
@@ -1,11 +1,17 @@
-DIR="build"
-if [ ! -d "$DIR" ]; then
-    mkdir ${DIR}
+BUILD_DIR="build"
+if [ ! -d "$BUILD_DIR" ]; then
+    mkdir ${BUILD_DIR}
+fi
+
+DIST_DIR="dist"
+if [ ! -d "$DIST_DIR" ]; then
+    mkdir ${DIST_DIR}
 fi
 
 cd build
 emcmake cmake ..
 emmake make
+
 em++ -O2 ./CMakeFiles/wasm-cppjieba.dir/src/wasm-cppjieba.cpp.o \
      -o ../dist/index.js \
      -s FORCE_FILESYSTEM=1 \
@@ -14,4 +20,5 @@ em++ -O2 ./CMakeFiles/wasm-cppjieba.dir/src/wasm-cppjieba.cpp.o \
      --pre-js ../src/wasm-cppjieba-pre.js \
      -s EXPORTED_FUNCTIONS="['_main', '_checkDict', '_initJiebaInstance', '_cutSentence']" \
      -s ALLOW_MEMORY_GROWTH=1
+
 emrun --no_browser --port 8081 ..
